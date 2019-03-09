cfb639cd4c3df60f1709e3a06096f0b8e8f9a8ba
Alexis Duran <alexis.duranm@gmail.com>
2019-03-09 13:36:37 +0100

    feat(DebtcollectiveHeader): Uncomment profile related components

--- src/DebtcollectiveHeader.js
+++ src/DebtcollectiveHeader.js
@@ -4,21 +4,28 @@ import AppBar from "./appbar";
 import Menu from "./menu";
 import React from "react";
 import Session from "./session";
+import {
+  AlertsDropdown,
+  InboxDropdown,
+  ProfileItems,
+  UserAvatarDropdown,
+} from "./profile";
 
 const DebtcollectiveHeader = ({ links }: { links: Array<Link> }) => (
   <AppBar>
     <Menu links={links} />
     <Session>
-      {({ user }) => JSON.stringify(user)
-      /* <Notifications user={user}>
-					{({ alerts, messages }) => (
-						<ProfileItems>
-							<UserAvatarDropdown user={user} />
-							<AlertsDropdown alerts={alerts} />
-							<InboxDropdown messages={messages} />
-						</ProfileItems>
-					)}
-				</Notifications> */
+      {({ user }) => (
+        //<Notifications user={user}>
+        // ({ alerts, messages }) => (
+        <ProfileItems>
+          <AlertsDropdown /* alerts={alerts} */ />
+          <InboxDropdown /* messages={messages} */ />
+          <UserAvatarDropdown user={user} />
+        </ProfileItems>
+      )
+      // )
+      //</Notifications>
       }
     </Session>
   </AppBar>
