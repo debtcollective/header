58a5309baddf9a9b06310eeb88bc0e3cd1a951e2
Alexis Duran <alexis.duranm@gmail.com>
2019-03-15 08:28:26 +0100

    refactor(session): Change SessionService to not use a class

--- src/session/SessionService.js
+++ src/session/SessionService.js
@@ -3,24 +3,28 @@
 import DiscourseService from "../common/DiscourseService";
 import get from "lodash/get";
 
-export default class SessionService {
-  static async getUser() {
-    try {
-      const response = await DiscourseService.get("session/current.json");
+const login = () => {
+  DiscourseService.goTo("login");
+};
 
-      return get(response, "current_user", null);
-    } catch (e) {
-      console.error(e);
+const signup = () => {
+  DiscourseService.goTo("signup");
+};
 
-      return null;
-    }
+const getUser = async (): Promise<?User> => {
+  try {
+    const response = await DiscourseService.get("session/current.json");
+
+    return get(response, "current_user", null);
+  } catch (e) {
+    console.error(e);
+
+    return null;
   }
+};
 
-  static login() {
-    DiscourseService.goTo("login");
-  }
-
-  static signup() {
-    DiscourseService.goTo("signup");
-  }
-}
+export default {
+  getUser,
+  login,
+  signup,
+};
