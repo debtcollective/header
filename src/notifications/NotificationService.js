59d35ea4b84e6bf208cf5dc6ffed973e097fe62f
Alexis Duran <alexis.duranm@gmail.com>
2019-03-24 15:03:57 +0100

    feat(notifications): Add logic for service to get recent notifications

--- src/notifications/NotificationService.js
+++ src/notifications/NotificationService.js
@@ -1,4 +1,21 @@
-const getNotifications = async () => null;
+// @flow
+
+import DiscourseService from "../common/DiscourseService";
+import get from "lodash/get";
+
+const getNotifications = async (
+  { limit }: { limit: number } = { limit: 20 }
+): Promise<Array<$NonMaybeType<Notification>>> => {
+  try {
+    const response = await DiscourseService.get(
+      `notifications.json?recent=true&limit=${limit}`
+    );
+
+    return get(response, "notifications", []);
+  } catch (e) {
+    return [];
+  }
+};
 
 export default {
   getNotifications,
---
+++ src/notifications/__tests__/NotificationService.spec.js
@@ -0,0 +1,36 @@
+import DiscourseService from "../../common/DiscourseService";
+import { notifications } from "./__fixtures__/notifications";
+import NotificationService from "../NotificationService";
+
+describe("NotificationService", () => {
+  describe("getNotifications", () => {
+    it("allows to send a request to get user recent notifications", () => {
+      DiscourseService.get = jest.fn().mockResolvedValueOnce(null);
+      NotificationService.getNotifications();
+
+      expect(DiscourseService.get).toHaveBeenNthCalledWith(
+        1,
+        "notifications.json?recent=true&limit=20"
+      );
+    });
+
+    it("returns the notifications when request is successful", done => {
+      const response = { notifications };
+
+      DiscourseService.get = jest.fn().mockResolvedValueOnce(response);
+      NotificationService.getNotifications().then(result => {
+        expect(result).toEqual(notifications);
+        done();
+      });
+    });
+
+    it("returns null when request is unsuccessful", done => {
+      DiscourseService.get = jest.fn().mockRejectedValueOnce("Network Error");
+
+      NotificationService.getNotifications().then(result => {
+        expect(result).toEqual([]);
+        done();
+      });
+    });
+  });
+});
