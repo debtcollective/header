e2242d6130a74e69a3177ba760d2650e89e15ef0
Alexis Duran <alexis.duranm@gmail.com>
2019-03-24 12:50:36 +0100

    refactor(notifications): Move notifications data to dedicated __fixtures__ folder

--- src/notifications/__tests__/Notifications.spec.js
+++ src/notifications/__tests__/Notifications.spec.js
@@ -3,6 +3,7 @@ jest.mock("../NotificationService");
 
 import "jest-dom/extend-expect";
 import { Notifications } from "../Notifications";
+import notificationsData from "./__fixtures__/notifications";
 import NotificationService from "../NotificationService";
 import React from "react";
 import { cleanup, render, waitForElement } from "react-testing-library";
@@ -20,44 +21,6 @@ const renderNotifications = (props: Object) => {
       }}
     </Notifications>
   );
-};
-
-const notificationsData = {
-  notifications: [
-    {
-      created_at: "2019-02-16T14:37:18.891Z",
-      data: {
-        display_username: "system",
-        group_id: 1,
-        topic_title: "Backup failed",
-      },
-      fancy_title: "Backup failed",
-      id: 1,
-      notification_type: 7,
-      post_number: 1,
-      read: false,
-      slug: "backup-failed",
-      topic_id: 11,
-    },
-    {
-      created_at: "2019-03-24T10:24:32.495Z",
-      data: {
-        display_username: "janedoe",
-        original_post_id: 41,
-        original_post_type: 1,
-        original_username: "janedoe",
-        revision_number: null,
-        topic_title: "Hello, I'm Jane Doe",
-      },
-      fancy_title: "Hello, I&rsquo;m Jane Doe",
-      id: 18,
-      notification_type: 6,
-      post_number: 1,
-      read: false,
-      slug: "hello-im-jane-doe",
-      topic_id: 26,
-    },
-  ],
 };
 
 describe("<Notifications />", () => {
