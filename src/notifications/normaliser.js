58d12899244a30edd4038c3affff27bacbe83b43
Alexis Duran <alexis.duranm@gmail.com>
2019-03-24 14:24:13 +0100

    refactor(notifications): Use lodash includes to ensure IE support

--- src/notifications/normaliser.js
+++ src/notifications/normaliser.js
@@ -1,5 +1,7 @@
 // @flow
 
+import includes from "lodash/includes";
+
 /**
  * check discourse notification types at: https://goo.gl/Lcyhp3
  * besides, keep into account that Discourse return `notification_type`
@@ -38,7 +40,7 @@ export const normaliseUserNotifications = (
   notifications: Array<Notification>
 ): { alerts: Array<Alert>, messages: Array<Message> } => {
   const messages = getMessages(notifications);
-  const alerts = notifications.filter(n => !messages.includes(n));
+  const alerts = notifications.filter(n => !includes(messages, n));
 
   return {
     alerts,
