38ac3b260c526b597e8ac4c221a04b13e07fa789
Alexis Duran <alexis.duranm@gmail.com>
2019-04-06 20:17:31 +0200

    feat(notifications): Add extra filter to normaliser to return only unread notifications
    Discourse doesn't provide a way to ask only for unread notifications, instead, it provides a way to
    know if there is unread notifications and then pull the recent ones which lead to the need to having
    a way to further filter those notifications to show only unread ones after get all

--- src/notifications/__tests__/normaliser.spec.js
+++ src/notifications/__tests__/normaliser.spec.js
@@ -10,4 +10,14 @@ describe("normaliser", () => {
     expect(result.alerts).toEqual([notifications[1], notifications[3]]);
     expect(result.messages).toEqual([notifications[0], notifications[2]]);
   });
+
+  it("returns only unread notifications", () => {
+    const notificationsUpdated = [...notifications];
+    notificationsUpdated[3].read = true;
+    notificationsUpdated[2].read = true;
+    const result = normaliseUserNotifications(notificationsUpdated);
+
+    expect(result.alerts).toEqual([notifications[1]]);
+    expect(result.messages).toEqual([notifications[0]]);
+  });
 });
