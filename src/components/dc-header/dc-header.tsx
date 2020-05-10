18b795e191e99443ad13c15cf17a1a0891a4bd27
Alexis Duran <alexis.duranm@gmail.com>
2020-05-10 13:35:21 +0200

    feat(header): render avatar when user logged in

--- src/components/dc-header/dc-header.tsx
+++ src/components/dc-header/dc-header.tsx
@@ -1,8 +1,21 @@
 import { Component, Prop, State, h, Watch, getAssetPath } from "@stencil/core";
 import { syncCurrentUser } from "../../services/session";
 
+type User = {
+  id: number;
+  admin: boolean;
+  avatar_template: string;
+  username: string;
+};
+
+const preffixDiscourse = (str) => `http://lvh.me:3000/${str}`;
+
+const getAvatarURL = ({ avatar_template }) => {
+  return preffixDiscourse(avatar_template.replace(`{size}`, 64));
+};
+
 @Component({
-  assetsDirs: ['assets'],
+  assetsDirs: ["assets"],
   tag: "dc-header",
   styleUrl: "dc-header.css",
   shadow: true,
@@ -19,23 +32,23 @@ export class Header {
   @Prop() links: string;
 
   /**
-   * An object with the user data when there is a 
-   * user logged in
+   * An object with the user data. Follows Discourse structure as
+   * https://docs.discourse.org/#tag/Users/paths/~1users~1{username}.json/get
    */
-  @State() user: Object = {};
+  @State() user: User;
 
   /**
    * Hos the value of "links" parsed to an actual Array
    */
   private _links: Array<{ text: string; href: string }>;
 
-  @Watch('links')
+  @Watch("links")
   linksDidChangeHandler(newValue) {
-    this._links = JSON.parse(newValue)
+    this._links = JSON.parse(newValue);
   }
 
   async syncCurrentUser() {
-    const user = await syncCurrentUser('http://lvh.me:3000')
+    const user = await syncCurrentUser("http://lvh.me:3000");
     this.user = user;
   }
 
@@ -45,15 +58,36 @@ export class Header {
   }
 
   render() {
+    const user = this.user;
+    const avatarURL = getAvatarURL(user);
+    const profileURL = preffixDiscourse(user.username);
+
     return (
       <header class="header">
-        <img class="logo" src={getAssetPath(`./assets/${this.logo}`)} alt="The Debtcollective"/>
+        <img
+          class="logo"
+          src={getAssetPath(`./assets/${this.logo}`)}
+          alt="The Debtcollective"
+        />
         <nav class="nav">
           {this._links.map(({ text, href }) => (
             <div class="nav-item">
-              <a class="nav-link" href={href}>{text}</a>
+              <a class="nav-link" href={href}>
+                {text}
+              </a>
             </div>
           ))}
+
+          <a id="current-user" href={profileURL} class="header-dropdown-toggle">
+            <img
+              alt="Profile picture"
+              width="32"
+              height="32"
+              src={avatarURL}
+              title={user.username}
+              class="avatar"
+            />
+          </a>
         </nav>
       </header>
     );
